c45cf3d1095e3e3504b8a638171fa0e9596b9bd3
prasbatman <prasath.skcet@gmail.com>
2020-04-09 10:09:46 +0200

    AI commit

--- Editor/src/DataObserver.java
+++
@@ -1,7 +0,0 @@
-//package editor.mcm.fhooe.at;
-
-import java.awt.image.BufferedImage;
-
-public interface DataObserver {
-	public void update(BufferedImage _data);
-}
--- Editor/src/Shape.java
+++
@@ -1,8 +0,0 @@
-//package editor.mcm.fhooe.at;
-
-import java.awt.Graphics2D;
-
-public interface Shape extends java.awt.Shape {
-	public String getText();
-	public void draw(Graphics2D g);
-}
